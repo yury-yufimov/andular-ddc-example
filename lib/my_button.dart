1610f52b903bb9b9da281402dcc11f57cf401858
yury.yufimov <yury.yufimov@team.wrike.com>
2019-12-12 18:13:39 +0300

    Extra imports to reduce number of generated files

--- lib/my_button.dart
+++ lib/my_button.dart
@@ -1,1 +1,5 @@
+// Uncomment to reduce number of *.ddc.js files
+//import 'package:angular_ddc_example/src/my_button/my_button.template.dart';
+//import 'package:angular_ddc_example/my_button.template.dart';
+
 export 'package:angular_ddc_example/src/my_button/my_button.dart';
