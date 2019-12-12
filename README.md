260329c2097111c4cabec10308d671bc1e1e4119
yury.yufimov <yury.yufimov@team.wrike.com>
2019-12-12 17:45:19 +0300

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # angular_ddc_example
 
+To compile project your can use command  
+`pub global run webdev serve`
+
+To see all compiled js files:  
+`find .dart_tool/build/generated -name "*.ddc.js"`
--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,7 @@
 import 'package:angular/angular.dart';
-import 'package:angular_ddc_example/app_component.template.dart' as ng;
+
+import 'demo_component/demo_component.template.dart' as ng;
 
 void main() {
-  runApp(ng.AppComponentNgFactory);
+  runApp(ng.DemoComponentNgFactory);
 }
